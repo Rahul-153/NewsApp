00fe3c942cba88c3aaa66593fe5c80b2be00d320
Rahul-153 <rathorerahul355@gmail.com>
2023-04-21 12:42:08 +0530

    added detailed screen

--- lib/widgets/detailed_card.dart
+++ lib/widgets/detailed_card.dart
@@ -13,19 +13,27 @@ class DetailedCard extends StatelessWidget {
       required this.source});
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.network(img),
-            Text(title,style: const TextStyle(
-                              fontSize: 20,
-                              color: Color(0XFFf2f2f2),
-                              fontFamily: 'Roboto Slab')),
-            Text(source),
-            Text(description)
-          ],
-        ),
+    return  SafeArea(
+      child: Material(
+         type: MaterialType.transparency,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+              height: MediaQuery.of(context).size.height,
+              color: Color(0xFF464646),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(img,height: MediaQuery.of(context).size.height*0.5,fit: BoxFit.cover,),
+                  SizedBox(height: 10,),
+                  Text(source,style: const TextStyle(color: Color(0xFFbababa),fontSize: 20)),
+                  SizedBox(height: 10,),
+                  Text(description,style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0XFFf2f2f2),
+                                    fontFamily: 'Roboto Slab')),
+                ],
+              ),
+            ),
       ),
     );
   }
